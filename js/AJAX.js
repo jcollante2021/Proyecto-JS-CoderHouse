bf4b53b3f5329d97d6de63f72445d44fd4be7ff2
jcollante2021 <jcollante@outlook.com.ar>
2021-05-07 16:58:06 -0300

    Correccion Boton Lista de Precios AJAX

--- js/AJAX.js
+++ js/AJAX.js
@@ -7,13 +7,13 @@ function traerDatosJSON(){
         if(status === "success"){
             let misDatos = res;
             for (const dato of misDatos){
-                $("#divListaDePrecios").append(`<div class="btnPrecios"><p>${dato.nombre} [ $${dato.precio} ]</p></div>`)
+                $("#datosListadePrecios").append(`<div class="btnPrecios"><p>${dato.nombre} [ $${dato.precio} ]</p></div>`)
             }
-            $("#divListaDePrecios").append(`<div id="btnSalirPrecios">Volver</div>`)
         }
-        $("#btnSalirPrecios").click(() => {$("#overlayPrecios").css("visibility", "hidden")})
     })
 }
 
 $("#btnListaDePrecios").click(() => {$("#overlayPrecios").css("visibility", "visible")})
+
+$("#btnSalirPrecios").click(() => {$("#overlayPrecios").css("visibility", "hidden")})
 
